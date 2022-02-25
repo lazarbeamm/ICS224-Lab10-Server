2add295016e5a3cceb381c0cbb943792500969ef
Michael Horie <michaelhorie@shaw.ca>
2022-02-25 14:41:13 -0800

    Added comments and logging commands

--- Server/NetworkSupport.swift
+++ Server/NetworkSupport.swift
@@ -7,23 +7,45 @@
 
 import Foundation
 import MultipeerConnectivity
+import os
 
+/// Uniquely identifies the service.
+/// Make this unique to avoid interfering with other Multipeer services.
+/// Don't forget to update the project Info AND the project Info.plist property lists accordingly.
 let serviceType = "lab10"
 
+/// This structure is used at setup time to identify client needs to a server.
+/// Currently, it only contains an identifying message, but this can be expanded to contain version information and other data.
 struct Request: Codable {
+    /// An identifying message that is to be transmitted.
     var details: String
 }
 
+/// This class deals with matters relating to setting up Server and Client Multipeer services.
 class NetworkSupport: NSObject, ObservableObject, MCNearbyServiceAdvertiserDelegate, MCNearbyServiceBrowserDelegate, MCSessionDelegate {
-    var peerID: MCPeerID
-    var session: MCSession
+    /// The local peer identifier
+    private var peerID: MCPeerID
+    
+    /// The current session
+    private var session: MCSession
+
+    /// For a server, this allows access to the MCNearbyServiceAdvertiser; nil otherwise.
     var nearbyServiceAdvertiser: MCNearbyServiceAdvertiser?
+    
+    /// For a client, this allows access to the MCNearbyServiceBrowser; nil otherwise.
     var nearbyServiceBrowser: MCNearbyServiceBrowser?
     
+    /// Contains the list of connected peers.  Used by the client.
     @Published var peers: [MCPeerID] = [MCPeerID]()
+    
+    /// True if connected to a peer, false otherwise.
     @Published var connected = false
+    
+    /// Contains the most recent incoming message.
     @Published var incomingMessage = ""
     
+    /// Create a Multipeer Server or Client
+    /// - Parameter browse: true creates a Client, false creates a Server
     init(browse: Bool) {
         peerID = MCPeerID(displayName: UIDevice.current.name)
         session = MCSession(peer: peerID, securityIdentity: nil, encryptionPreference: .none)
@@ -45,127 +67,158 @@ class NetworkSupport: NSObject, ObservableObject, MCNearbyServiceAdvertiserDeleg
         }
     }
     
-    // MARK: -
+    // MARK: - MCNearbyServiceAdvertiserDelegate Methods. See XCode documentation for details.
     
+    /// Inherited from MCNearbyServiceAdvertiserDelegate.advertiser(_:didNotStartAdvertisingPeer:).
+    /// Currently only logs.
     func advertiser(_ advertiser: MCNearbyServiceAdvertiser, didNotStartAdvertisingPeer error: Error) {
-        print("didNotStartAdvertisingPeer", error)
+        os_log("didNotStartAdvertisingPeer \(error.localizedDescription)")
     }
     
+    /// Inherited from MCNearbyServiceAdvertiserDelegate.advertiser(_:didReceiveInvitationFromPeer:withContext:invitationHandler:).
+    /// Right now, all connection requests are accepted.
     func advertiser(_ advertiser: MCNearbyServiceAdvertiser, didReceiveInvitationFromPeer peerID: MCPeerID, withContext context: Data?, invitationHandler: @escaping (Bool, MCSession?) -> Void) {
         do {
             let request = try JSONDecoder().decode(Request.self, from: context ?? Data())
-            print("didReceiveInvitationFromPeer", peerID, request)
+            os_log("didReceiveInvitationFromPeer \(peerID.displayName) \(request.details)")
+            
             invitationHandler(true, self.session)
         }
         catch let error {
-            print("didReceiveInvitationFromPeer", error)
+            os_log("didReceiveInvitationFromPeer \(error.localizedDescription)")
         }
     }
     
-    // MARK: -
+    // MARK: - MCNearbyServiceBrowserDelegate Methods. See XCode documentation for details.
     
+    /// Inherited from MCNearbyServiceBrowserDelegate.browser(_:didNotStartBrowsingForPeers:).
+    /// Currently only logs.
     func browser(_ browser: MCNearbyServiceBrowser, didNotStartBrowsingForPeers error: Error) {
-        print("didNotStartBrowsingForPeers", error)
+        os_log("didNotStartBrowsingForPeers \(error.localizedDescription)")
     }
     
+    /// Inherited from MCNearbyServiceBrowserDelegate.browser(_:foundPeer:withDiscoveryInfo:).
+    /// Updates the peers array with the newly-found peerID.
     func browser(_ browser: MCNearbyServiceBrowser, foundPeer peerID: MCPeerID, withDiscoveryInfo info: [String : String]?) {
-        print("foundPeer", peerID, info ?? "")
+        os_log("foundPeer", peerID, info ?? "")
         if !peers.contains(peerID) {
             peers.append(peerID)
         }
     }
     
+    /// Inherited from MCNearbyServiceBrowserDelegate.browser(_:lostPeer:).
+    /// Removes the lost peerID from the peers array.
     func browser(_ browser: MCNearbyServiceBrowser, lostPeer peerID: MCPeerID) {
-        print("lostPeer", peerID, terminator: " ")
         guard let index = peers.firstIndex(of: peerID) else {
-            print("NOT FOUND")
+            os_log("lostPeer \(peerID) NOT FOUND")
             return
         }
         peers.remove(at: index)
-        print("removed")
+        os_log("lostPeer \(peerID) removed")
     }
     
-    // MARK: -
+    // MARK: - Client Setup Method
     
+    /// Establish a session with a server.
+    /// - Parameters:
+    ///   - peerID: The peer ID of the server.
+    ///   - request: The connection request.  This can contain additional information that can be used by a server to accept or reject a connection.
     func contactPeer(peerID: MCPeerID, request: Request) throws {
-        print("contactPeer", peerID, request)
+        os_log("contactPeer \(peerID) \(request.details)")
         let request = try JSONEncoder().encode(request)
         nearbyServiceBrowser?.invitePeer(peerID, to: session, withContext: request, timeout: TimeInterval(120))
     }
     
-    // MARK: -
+    // MARK: - MCSessionDelegate Methods. See XCode documentation for details.
     
+    /// Inherited from MCSessionDelegate.session(_:didReceive:fromPeer:).
+    /// Updates incomingMessage with the message that was just received
     func session(_ session: MCSession, didReceive: Data, fromPeer: MCPeerID) {
         do {
             let request = try JSONDecoder().decode(String.self, from: didReceive)
-            print("didReceive", request, fromPeer)
+            os_log("didReceive \(request) \(fromPeer)")
             DispatchQueue.main.async {
                 self.incomingMessage = request
             }
         }
         catch let error {
-            print("didReceive", error)
+            os_log("didReceive \(error.localizedDescription)")
         }
     }
     
+    /// Inherited from MCSessionDelegate.session(_:didStartReceivingResourceWithName:fromPeer:with:).
+    /// Currently only logs.
     func session(_ session: MCSession, didStartReceivingResourceWithName: String, fromPeer: MCPeerID, with: Progress) {
-        print("didStartReceivingResourceWithName", didStartReceivingResourceWithName, fromPeer, with)
+        os_log("didStartReceivingResourceWithName \(didStartReceivingResourceWithName) \(fromPeer) \(with)")
     }
     
+    /// Inherited from MCSessionDelegate.session(_:didFinishReceivingResourceWithName:fromPeer:at:withError:).
+    /// Currently only logs.
     func session(_ session: MCSession, didFinishReceivingResourceWithName: String, fromPeer: MCPeerID, at: URL?, withError: Error?) {
-        print("didFinishReceivingResourceWithName", didFinishReceivingResourceWithName, fromPeer, at ?? "", withError ?? "")
+        let at2 = at?.description ?? ""
+        let withError2 = withError?.localizedDescription ?? ""
+        os_log("didFinishReceivingResourceWithName \(didFinishReceivingResourceWithName) \(fromPeer) \(at2) \(withError2)")
     }
     
+    /// Inherited from MCSessionDelegate.session(_:didReceive:withName:fromPeer:).
+    /// Currently only logs.
     func session(_ session: MCSession, didReceive: InputStream, withName: String, fromPeer: MCPeerID) {
-        print("didReceive:withName", didReceive, withName, fromPeer)
+        os_log("didReceive:withName \(didReceive) \(withName) \(fromPeer)")
     }
     
+    /// Inherited from MCSessionDelegate.session(_:peer:didChange:).
+    /// Updates the connected state.
     func session(_ session: MCSession, peer: MCPeerID, didChange: MCSessionState) {
         switch didChange {
         case .notConnected:
-            print("didChange notConnected", peer)
+            os_log("didChange notConnected \(peer)")
             DispatchQueue.main.async {
                 self.connected = false
             }
         case .connecting:
-            print("didChange connecting", peer)
+            os_log("didChange connecting \(peer)")
             DispatchQueue.main.async {
                 self.connected = false
             }
         case .connected:
-            print("didChange connected", peer)
+            os_log("didChange connected \(peer)")
             DispatchQueue.main.async {
                 self.connected = true
             }
         default:
-            print("didChange", peer)
+            os_log("didChange \(peer)")
             DispatchQueue.main.async {
                 self.connected = false
             }
         }
     }
     
+    /// Inherited from MCSessionDelegate.session(_:didReceiveCertificate:fromPeer:certificateHandler:).
+    /// Currently accepts all certificates.
     func session(_ session: MCSession, didReceiveCertificate: [Any]?, fromPeer: MCPeerID, certificateHandler: (Bool) -> Void) {
-        print("didReceiveCertificate", didReceiveCertificate ?? "", fromPeer)
+        os_log("didReceiveCertificate", didReceiveCertificate ?? "", fromPeer)
         certificateHandler(true)
     }
     
-    // MARK: -
+    // MARK: - Data Transmission Method
     
+    /// Sends a message to all registered peers.  Used by the client.
+    /// - Parameter message: The message that is to be transmitted.
     func send(message: String) {
-        print("send", message)
         do {
             let data = try JSONEncoder().encode(message)
             try session.send(data, toPeers: peers, with: .reliable)
+            os_log("send \(message)")
         }
         catch let error {
-            print(error)
+            os_log("send \(error.localizedDescription)")
         }
     }
     
-    // MARK: -
+    // MARK: - Cleanup
+    
     deinit {
-        print("deinit")
+        os_log("deinit")
         nearbyServiceBrowser?.stopBrowsingForPeers()
     }
 }
